bf4a8fd0ac6f518e47b1d10c904e4a4683618e11
Filipe Araujo <luiz4r4ujo@gmail.com>
2024-07-14 22:26:02 -0300

    User entity and resource

---
+++ src/main/java/com/purplehaze/course/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.purplehaze.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.purplehaze.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User user = new User(1, "Jimi", "jimi@gmail.com", "09090909", "12345");
+		return ResponseEntity.ok().body(user);
+	}
+
+}
