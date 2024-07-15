53dfc8a0a5a364f1ecbc77e28f6e897a822d177d
Filipe Araujo <luiz4r4ujo@gmail.com>
2024-07-15 18:57:03 -0300

    H2 database, test profile, JPA

--- src/main/java/com/purplehaze/course/entities/User.java
+++ src/main/java/com/purplehaze/course/entities/User.java
@@ -3,9 +3,19 @@ package com.purplehaze.course.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table (name = "td_user")
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	private String name;
 	private String email;
