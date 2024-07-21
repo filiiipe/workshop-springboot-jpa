9fea770bb6d57d63fc25d7eb2ec9517b9b84886c
Filipe Araujo <luiz4r4ujo@gmail.com>
2024-07-21 19:00:36 -0300

    Service layer, component registration

--- src/main/java/com/purplehaze/course/resources/UserResource.java
+++ src/main/java/com/purplehaze/course/resources/UserResource.java
@@ -1,20 +1,34 @@
 package com.purplehaze.course.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.purplehaze.course.entities.User;
+import com.purplehaze.course.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService userService;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User user = new User(null, "Jimi", "jimi@gmail.com", "09090909", "12345");
-		return ResponseEntity.ok().body(user);
+	public ResponseEntity<List<User>> findAll(){
+		List<User> list = userService.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User obj = userService.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 
 }
