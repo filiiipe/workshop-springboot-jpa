d1b37460208ceee34534ac4328938d8e63531af0
Filipe Araujo <luiz4r4ujo@gmail.com>
2024-08-11 00:42:01 -0300

    User update

--- src/main/java/com/purplehaze/course/services/UserService.java
+++ src/main/java/com/purplehaze/course/services/UserService.java
@@ -31,4 +31,17 @@ public class UserService {
 	public void delete(Long id) {
 		userRepository.deleteById(id);
 	}
+	
+	public User update(Long id, User obj) {
+		User entity = userRepository.getReferenceById(id);
+		updateData(entity, obj);
+		return userRepository.save(entity);
+	}
+
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+	}
+	
 }
