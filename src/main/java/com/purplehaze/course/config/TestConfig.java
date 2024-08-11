8ebd88701466b726aa8f37e44e06df8917acda6b
Filipe Araujo <luiz4r4ujo@gmail.com>
2024-08-10 23:29:01 -0300

    Payment, one-to-one association

--- src/main/java/com/purplehaze/course/config/TestConfig.java
+++ src/main/java/com/purplehaze/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.purplehaze.course.entities.Category;
 import com.purplehaze.course.entities.Order;
 import com.purplehaze.course.entities.OrderItem;
+import com.purplehaze.course.entities.Payment;
 import com.purplehaze.course.entities.Product;
 import com.purplehaze.course.entities.User;
 import com.purplehaze.course.entities.enums.OrderStatus;
@@ -80,7 +81,10 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
-		
-	}
 	
+		Payment pay = new Payment(null, Instant.parse("2019-07-21T05:42:10Z"), o2);
+		o2.setPayment(pay);
+		
+		orderRepository.save(o2);
+	}
 }
