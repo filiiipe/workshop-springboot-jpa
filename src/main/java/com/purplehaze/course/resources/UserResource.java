231e33fa0107a636c3c2463c14774990e9154df6
Filipe Araujo <luiz4r4ujo@gmail.com>
2024-08-11 00:21:43 -0300

    User insert

--- src/main/java/com/purplehaze/course/resources/UserResource.java
+++ src/main/java/com/purplehaze/course/resources/UserResource.java
@@ -1,13 +1,17 @@
 package com.purplehaze.course.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.purplehaze.course.entities.User;
 import com.purplehaze.course.services.UserService;
@@ -30,5 +34,15 @@ public class UserResource {
 		User obj = userService.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
+	
+	@PostMapping
+	public ResponseEntity<User> Insert(@RequestBody User obj){
+		obj = userService.Insert(obj);
+		URI uri = ServletUriComponentsBuilder.
+				fromCurrentRequest().path("/{id}")
+				.buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
+	
 
 }
