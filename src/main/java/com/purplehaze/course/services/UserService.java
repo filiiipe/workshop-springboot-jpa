70527fb1aa13e0e53f0a98bc421cea67cc3e8a25
Filipe Araujo <luiz4r4ujo@gmail.com>
2024-08-11 01:10:23 -0300

    Exception handling - delete

--- src/main/java/com/purplehaze/course/services/UserService.java
+++ src/main/java/com/purplehaze/course/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.purplehaze.course.entities.User;
 import com.purplehaze.course.repositories.UserRepository;
+import com.purplehaze.course.services.exceptions.DatabaseException;
 import com.purplehaze.course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -15,24 +18,31 @@ public class UserService {
 
 	@Autowired
 	private UserRepository userRepository;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return userRepository.findAll();
 	}
-	
+
 	public User findById(Long id) {
-		Optional <User> obj = userRepository.findById(id);
+		Optional<User> obj = userRepository.findById(id);
 		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
-	
+
 	public User Insert(User obj) {
 		return userRepository.save(obj);
 	}
-	
+
 	public void delete(Long id) {
-		userRepository.deleteById(id);
+		try {
+			userRepository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);	
+		} 
+		catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
-	
+
 	public User update(Long id, User obj) {
 		User entity = userRepository.getReferenceById(id);
 		updateData(entity, obj);
@@ -44,5 +54,5 @@ public class UserService {
 		entity.setEmail(obj.getEmail());
 		entity.setPhone(obj.getPhone());
 	}
-	
+
 }
